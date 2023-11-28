13f6969a9babcc030dc1ec72dcd87ef07ead2682
OCensoredO <pgvdpb@gmail.com>
2023-11-28 17:36:48 +0900

    코드 최적화

--- My project/Assets/Scripts/MonoBehavior/Boss.cs
+++ My project/Assets/Scripts/MonoBehavior/Boss.cs
@@ -10,6 +10,7 @@ public class Boss : MonoBehaviour
 
     private DataManager dMan;
     private new Renderer renderer;
+    private LineRenderer lineRenderer;
     //private GameObject playerObj;
 
     private FSM<BMsg> bossFSM;
@@ -24,6 +25,7 @@ public class Boss : MonoBehaviour
 
         // 시연용 기능인 SetColor()를 위해 임시로 선언한 변수
         renderer = GetComponent<Renderer>();
+        lineRenderer = GetComponent<LineRenderer>();
 
         bossFSM = new FSM<BMsg>();
         bossFSM.Start(new IdleBossState(this));
@@ -103,8 +105,12 @@ public class Boss : MonoBehaviour
     private IEnumerator shootBeam()
     {
         float startTime = Time.time;
-        float originalSpeed = speed;
 
+        // 패턴 사용 전 이동 속도 임시 변수에 저장, 패턴 사용 중 이동을 잠시 멈추게끔 스피드를 0으로 줌
+        float originalSpeed = speed;
+        speed = 0f;
+
+        // 빔 프리팹 로드 및 인스턴스 생성
         GameObject beamPrefab = Resources.Load(dMan.gameData.enemyBullets[0].prefab) as GameObject;
         GameObject beamInstance = Instantiate(beamPrefab, transform.position, transform.rotation);
 
@@ -112,20 +118,21 @@ public class Boss : MonoBehaviour
         ray.origin = transform.position - new Vector3(0f, 0f, transform.localScale.z * 0.51f);
         Vector3 targetPos = transform.position + new Vector3(0f, -5f, -0.4f);
 
-        speed = 0f;
 
-        // 빔 발사
+        // 일정 시간동안 빔 발사
         while (Time.time - startTime < 2.7f)
         {
             targetPos.y += 2.2f * Time.deltaTime;
             ray.direction = targetPos;
-            //beamInstance.transform.position = ray.origin;
             beamInstance.transform.LookAt(targetPos);
 
             yield return null;
         }
 
+        // 전진 속도 원래대로 돌려놓기
         speed = originalSpeed;
+
+        // 빔 인스턴스 제거
         Destroy(beamInstance);
         yield return null;
     }
@@ -153,103 +160,77 @@ public class Boss : MonoBehaviour
     {
         // 코드 개선 필요
         int repeatedCount = 0;
-        //float timer = 0.5f;
         float startTime = Time.time;
-        //float currTime = Time.time;
 
+        // 인스턴스 로드
         GameObject beamPrefab = Resources.Load(dMan.gameData.enemyBullets[0].prefab) as GameObject;
-        Vector3? targetPos = null;
 
-        Vector3? prevPos = null;
-        Vector3? currPos = null;
+        // 저격할 좌표, 직전 프레임/현 프레임에서의 플레이어 위치를 저장할 변수 선언
+        Vector3 targetPos;
+        Vector3 prevPos;
+        Vector3 currPos;
+
         GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
         PlayerController pCon = playerObj.GetComponent<PlayerController>();
 
         GameObject beamInstance = null;
 
+        prevPos = playerObj.transform.position;
+        yield return null;
+
+        // 조준-빔 발사 3회 반복
         while (repeatedCount < 3)
         {
-            if (!(beamInstance is null)) Destroy(beamInstance);
-
+            // 조준
+            /*
+            // 첫 루프 때의 예외 상황(prevPos가 업데이트되지 않음) 처리
             if (!prevPos.HasValue)
             {
-                prevPos = playerObj.transform.position;
-                yield return null;
+                
             }
+            */
+
+            // 현 프레임에서의 플레이어 위치 구해서 currPos에 저장
             currPos = playerObj.transform.position;
 
-            // 예측 이동 방향 구하기
-            Vector3 predictedPlayerDirection = new Vector3();
-            predictedPlayerDirection.x = Input.GetAxisRaw("Horizontal");
-            predictedPlayerDirection.y = ((Vector3)(currPos - prevPos)).normalized.y;
-            predictedPlayerDirection.z = Input.GetAxisRaw("Vertical");
-
+            // 예측 이동 방향을 구해 플레이어가 이동할 위치 예측
+            Vector3 predictedPlayerDirection = (currPos - prevPos).normalized;
             targetPos = predictPosWithGravity(predictedPlayerDirection, playerObj.transform.position, pCon.GetYVel(), pCon.GetPlayerSpeed(), 0.4f);
 
-            // 예측 위치를 추적하는 레이 생성
+            // 예측 위치를 추적하는 레이 생성하여 직선으로 렌더링
             Ray ray = new Ray();
             ray.origin = transform.position - new Vector3(0f, 0f, transform.localScale.z * 0.51f);
-            ray.direction = (Vector3)targetPos;
+            ray.direction = targetPos;
+            renderLine(ray.origin, targetPos);
 
-            // 디버깅하기 위해 레이를 직선으로 렌더링
-            renderLine(ray.origin, (Vector3)targetPos);
+            // prevPos 업데이트
+            prevPos = currPos;
 
-            prevPos = playerObj.transform.position;
+            // 빔 발사 전까지 일정 시간 대기
             yield return new WaitForSeconds(0.7f);
 
-            //Debug.Log("Shoot");
+            // 빔 발사
+            // 빔 인스턴스 생성 및 크기/회전 조정
             beamInstance = Instantiate(beamPrefab, transform.position, transform.rotation);
             float z = beamInstance.gameObject.transform.localScale.z;
             beamInstance.gameObject.transform.localScale = new Vector3(0.1f, 0.1f, z);
-            beamInstance.transform.LookAt((Vector3)targetPos);
+            beamInstance.transform.LookAt(targetPos);
             
+            // 반복 횟수 1회 증가
             repeatedCount++;
+
+            // 일정 시간동안 발사한 빔 지속
             yield return new WaitForSeconds(0.7f);
             startTime = Time.time;
+            Destroy(beamInstance);
         }
 
-        Destroy(beamInstance);
+        //Destroy(beamInstance);
+        enableLineRenderer(false);
         yield return null;
     }
-
-    //Debug.Log(Time.time + ", " + (Time.time - startTime));
 
     /*
-    if (Time.time - startTime < 0.5f)
-    {
-        //Debug.Log("Aim");
-        if (!(beamInstance is null)) Destroy(beamInstance);
-
-        if (!prevPos.HasValue)
-        {
-            prevPos = playerObj.transform.position;
-            yield return null;
-        }
-        currPos = playerObj.transform.position;
-
-        // 예측 이동 방향 구하기
-        Vector3 predictedPlayerDirection = new Vector3();
-        predictedPlayerDirection.x = Input.GetAxisRaw("Horizontal");
-        predictedPlayerDirection.y = ((Vector3)(currPos - prevPos)).normalized.y;
-        predictedPlayerDirection.z = Input.GetAxisRaw("Vertical");
-
-        targetPos = predictPosWithGravity(predictedPlayerDirection, playerObj.transform.position, pCon.GetYVel(), pCon.GetPlayerSpeed(), 0.3f);
-
-        // 예측 위치를 추적하는 레이 생성
-        Ray ray = new Ray();
-        ray.origin = transform.position - new Vector3(0f, 0f, transform.localScale.z * 0.51f);
-        ray.direction = (Vector3)targetPos;
-
-        // 디버깅하기 위해 레이를 직선으로 렌더링
-        renderLine(ray.origin, (Vector3)targetPos);
-
-        prevPos = playerObj.transform.position;
-        yield return null;
-        continue;
-    }
-    */
-    //Debug.Log("Aim");
-
     private IEnumerator aim()
     {
         // 1틱 전 위치와 현재 위치를 비교해 다음 좌표를 예측
@@ -287,39 +268,20 @@ public class Boss : MonoBehaviour
             yield return null;
         }
     }
+    */
 
     public void GetClosertoPlayer()
     {
         transform.Translate(0f, 0f, -speed * Time.deltaTime);
     }
 
-    /*
-    public Vector3 predictPos(Vector3 playerDirection, float delaySecond)
-    {
-        // 딜레이 = delaySecond / deltatime
-        // 속력 = 플레이어 스피드(일단 대충 줌)
-        float predictedXVel = 16f * playerDirection.x;
-
-        float predictedYVel = 0f;
-        float currYVel = playerObj.GetComponent<Rigidbody>().velocity.y;
-        if (currYVel != 0f) predictedYVel = currYVel + Physics.gravity.y * delaySecond;
-
-        float predictedZVel = 16f * playerDirection.z;
-
-        Vector3 predictedPlayerPos = playerObj.transform.position + new Vector3(predictedXVel, predictedYVel, predictedZVel) * delaySecond;
-        if (predictedPlayerPos.y < 0f) predictedPlayerPos.y = 0f;
-        //return playerObj.transform.position + new Vector3(16f * playerDirection.x * delaySecond, 0, 16f * playerDirection.z * delaySecond);
-        return predictedPlayerPos;
-    }
-    */
-
     public Vector3 predictPosWithGravity(Vector3 direction, Vector3 currPos, float currYVel, float movingSpeed, float delaySecond)
     {
         // 주어진 속력과 방향을 기반으로 예측 속도 구하기
-        float predictedXVel = movingSpeed * direction.x;
+        float predictedXVel = movingSpeed * direction.x * Time.fixedDeltaTime;
         float predictedYVel = 0f;
         if (currYVel != 0f) predictedYVel = currYVel + Physics.gravity.y * delaySecond;
-        float predictedZVel = movingSpeed * direction.z;
+        float predictedZVel = movingSpeed * direction.z * Time.fixedDeltaTime;
 
         Vector3 predictedPlayerPos = currPos + new Vector3(predictedXVel, predictedYVel, predictedZVel) * delaySecond;
         // 땅에 착지할 것으로 예측되는 경우 예측 위치의 y좌표를 0f로 조정
@@ -328,9 +290,10 @@ public class Boss : MonoBehaviour
         return predictedPlayerPos;
     }
 
+    // Ray 렌더링용으로 사용하는 함수
     private void renderLine(Vector3 pos0, Vector3 pos1)
     {
-        LineRenderer lineRenderer = GetComponent<LineRenderer>();
+        enableLineRenderer(true);
         lineRenderer.startColor = Color.red;
         lineRenderer.endColor = Color.yellow;
         lineRenderer.startWidth = 0.1f;
@@ -339,10 +302,13 @@ public class Boss : MonoBehaviour
         lineRenderer.SetPosition(1, pos1);
     }
 
+    private void enableLineRenderer(bool isEnabled) { lineRenderer.enabled = isEnabled; }
+    /*
     private int getSign(int value)
     {
         if (value > 0) return 1;
         if (value == 0) return 0;
         return -1;
     }
+    */
 }
